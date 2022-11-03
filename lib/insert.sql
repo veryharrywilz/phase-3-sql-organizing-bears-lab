c81267c33cb75857e9924c54323e0ff10f3ff52d
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-03 16:25:27 -0400

    commit tree d0e63a135346d241ab74a9fde47c91b8cf13a19e

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Docile", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "F", "Black", "Ramboncious", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Brown", "Kind", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, "M", "White", "Mean", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "White", "Sad", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 15, "M", "Grey", "Snooty", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 18, "F", "White", "Shy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "Brown", "Newborn", true);
+
+
